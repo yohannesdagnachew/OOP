0b867ee4dd6ed5de98e2a48d6534ed408b67c97f
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-08-31 11:35:03 +0300

    Add fix to linter errors

--- load_data.rb
+++ load_data.rb
@@ -1,29 +1,29 @@
 module LoadData
-    def load_book
-      if File.exist?('./books.json')
-        saved_books = JSON.parse(File.read('./books.json'))
-        books = []
-        saved_books.each do |book|
-          books << Book.new(book['title'], book['author'])
-        end
-        books
-      else
-        File.write('./books.json', '[]')
+  def load_book
+    if File.exist?('./books.json')
+      saved_books = JSON.parse(File.read('./books.json'))
+      books = []
+      saved_books.each do |book|
+        books << Book.new(book['title'], book['author'])
       end
-    end
-  
-    def load_person
-      saved_person = JSON.parse(File.read('./persons.json'))
-      people = []
-      saved_person.each do |person|
-        people << Person.new(person['id'], person['name'], person['age'])
-      end
-      people
-    end
-  
-    def load_rentals
-      saved_rentals = open('./rentals.json')
-      save = saved_rentals.read
-      rent = JSON.parse(save)
+      books
+    else
+      File.write('./books.json', '[]')
     end
   end
+
+  def load_person
+    saved_person = JSON.parse(File.read('./persons.json'))
+    people = []
+    saved_person.each do |person|
+      people << Person.new(person['id'], person['name'], person['age'])
+    end
+    people
+  end
+
+  def load_rentals
+    saved_rentals = open('./rentals.json')
+    save = saved_rentals.read
+    rent = JSON.parse(save)
+  end
+end
